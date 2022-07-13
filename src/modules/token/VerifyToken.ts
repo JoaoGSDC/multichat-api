23b40e6d667f8d6850e57775dc952f0a57c9b83d
João Cruz <joaogsdc@gmail.com>
2022-07-12 23:58:21 -0300

    fix: token adjustment

--- src/modules/token/VerifyToken.ts
+++ src/modules/token/VerifyToken.ts
@@ -1,11 +1,7 @@
-import { Request, Response, NextFunction } from "express";
+import { Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 
-interface IRequest extends Request {
-  userId: string;
-}
-
-export function verifyJWT(req: IRequest, res: Response, next: NextFunction) {
+export function verifyJWT(req: any, res: Response, next: NextFunction) {
   const token: string = req.headers["x-access-token"] as string;
   jwt.verify(token, process.env.SECRET ?? "", (err: any, decoded: any) => {
     if (err) return res.status(401).end();
